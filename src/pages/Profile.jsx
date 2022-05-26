80aa66e1db97380dbe6d6507a5c1f37021c61f6f
Zhongqi Yue <z3yue@uwaterloo.ca>
2022-05-26 13:46:29 -0400

    user add new listing, upload images, create new listing page, and set up GeoAuth from google

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -15,7 +15,8 @@ import {
 } from 'firebase/firestore'
 import { db } from '../firebase.config'
 import { toast } from 'react-toastify'
-
+import arrowRight from '../assets/svg/keyboardArrowRightIcon.svg'
+import homeIcon from '../assets/svg/homeIcon.svg'
 
 
 function Profile() {
@@ -106,6 +107,14 @@ function Profile() {
             />
           </form>
         </div>
+
+        <Link to='/create-listing' className='createListing'>
+          <img src={homeIcon} alt='home' />
+          <p>Sell or rent your home</p>
+          <img src={arrowRight} alt='arrow right' />
+        </Link>
+
+        
 
       </main>
 
