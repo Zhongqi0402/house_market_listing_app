d6b93cbe9cefd3a30d3f4e088894dc5846869c15
Zhongqi Yue <z3yue@uwaterloo.ca>
2022-05-27 17:02:09 -0400

    added in single listing and google map for displaying locations

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ import 'react-toastify/dist/ReactToastify.css'
 import PrivateRoute from './components/PrivateRoute';
 import Category from './pages/Category';
 import CreateListing from './pages/CreateListing';
+import Listing from './pages/Listing';
+import Contact from './pages/Contact';
+
 
 function App() {
   return (
@@ -29,6 +32,12 @@ function App() {
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/create-listing" element={<CreateListing />} />
+          <Route
+            path='/category/:categoryName/:listingId'
+            element={<Listing />}
+          />
+          <Route path="/contact/:landlordId" element={<Contact />} />
+
         </Routes>
 
         <Navbar />
